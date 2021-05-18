aa0e4ae3235ebb34a81e0f24d6b5ee30a41e7a32
Prateek Karnal <karnalprateek@gmail.com>
2021-05-18 15:22:07 +0530

    Update xpath

--- slot_notifier_config.py
+++ slot_notifier_config.py
@@ -3,8 +3,7 @@ SEARCH_STATE_XPATH="/html/body/app-root/div/app-home/div[2]/div/appointment-tabl
 SEARCH_DISTRICT_XPATH="/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div/div[2]/mat-form-field"
 SEARCH_BUTTON_XPATH="/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div/div[3]/button"
 FILTER_BUTTON_18_PLUS_XPATH="/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[3]/div/div[1]/label"
-SLOTS_TABLE_XPATH="/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[6]/div/div/div"
-
+SLOTS_TABLE_XPATH="/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[9]/div/div/div"
 CAPTCHA_INPUT_XPATH='//*[@id="main-content"]/app-appointment-table/ion-content/div/div/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row[2]/ion-col/div/div/div[2]/input'
 CAPTCHA_IMAGE_XPATH='//*[@id="captchaImage"]'
 TIME_SLOT_BUTTON_LIST_XPATH='//*[@id="main-content"]/app-appointment-table/ion-content/div/div/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[1]/div'
